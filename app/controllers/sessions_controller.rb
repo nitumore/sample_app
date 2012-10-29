ff3fce281f29934983c53ca49b908cab203b85d4
Nitin More <nmore2012@gmail.com>
2012-10-29 19:59:52 +0530

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,16 @@
 class SessionsController < ApplicationController
 
   def new
+    flash[:success] = "Inside new() of sessioncontroller"   
   	render 'new'
   end
 
  def create
+    flash[:success] = "Inside create() of sessioncontroller"   
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
@@ -16,6 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    flash[:success] = "Inside destroy() of sessioncontroller"  
   	sign_out
     redirect_to root_url
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -62,7 +62,7 @@ describe User do
     	it { should_not be_valid }
   end
 
-describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -120,4 +120,18 @@ describe "when email format is invalid" do
     its(:remember_token) { should_not be_blank }
   end
 
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 end
