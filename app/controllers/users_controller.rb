7d3eb03481cd5a9f3ee74fd5507a066e3ac9e084
Nitin More <nmore2012@gmail.com>
2012-10-31 14:47:18 +0530

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
-
+=begin
   def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+     if (current_user.admin? && (User.find(params[:id]) == current_user))
+
+      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
+     end 
   end
+=end
+  def show
+     
+      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
+    
+  end
+
 
   def new
     @user = User.new
@@ -37,15 +48,31 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+ 
+    def index
+      @users = User.paginate(page: params[:page])
+    end
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
 
   def destroy
       User.find(params[:id]).destroy
       flash[:success] = "User destroyed."
       redirect_to users_url
+  end
+
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
 
